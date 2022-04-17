604be4ce2e3b89de27d86b49aebdfb00417c65ef
Oleksandr Andreiev <Oleksandr.Andreiev@halliburton.com>
2022-04-17 17:03:56 +0200

    polished logic

--- TaxCalculator/Domain/Entities/TaxProfile.cs
+++ TaxCalculator/Domain/Entities/TaxProfile.cs
@@ -25,17 +25,7 @@ public class TaxProfile : BaseEntity
     public IEnumerable<TaxDataItemDto> CalculateTaxes(DateTime? from = null, DateTime? to = null)
     {
         var incomes = Incomes.ToArray();
-        if (from != null)
-        {
-            incomes = incomes.Where(x => x.CreatedDate.Month >= from.Value.Month 
-                                         && x.CreatedDate.Year == from.Value.Year).ToArray();
-        }
-
-        if (to != null)
-        {
-            incomes = incomes.Where(x => x.CreatedDate.Month <= to.Value.Month 
-                                         && x.CreatedDate.Year == to.Value.Year).ToArray();
-        }
+        ApplyDateFilter(ref incomes, from, to);
         
         var groupedIncomes = incomes.GroupBy(x => new
         {
@@ -43,28 +33,10 @@ public class TaxProfile : BaseEntity
             x.CreatedDate.Month
         });
 
-        var totalIncome = Incomes.Sum(x => x.Value);
-        var incomeTaxes = Taxes.Where(x => x.TaxType == TaxType.IncomeTax).ToArray();
-        if (!incomeTaxes.Any())
-        {
-            throw new Exception("No income taxes were configured");
-        }
-        
-        var incomeTax = incomeTaxes.Where(x => x.AppliesBefore != null && x.AppliesBefore < totalIncome)
-            .OrderByDescending(x => x.AppliesBefore)
-            .FirstOrDefault();
-        
-        if (incomeTax == null)
-        {
-            incomeTax = incomeTaxes.FirstOrDefault();
-        }
+        var totalIncome = incomes.Sum(x => x.Value);
 
+        var incomeTax = GetIncomeTax(totalIncome);
         var socialTax = Taxes.FirstOrDefault(x => x.TaxType == TaxType.SocialTax);
-
-        if (incomeTax == null)
-        {
-            incomeTax = incomeTaxes.FirstOrDefault();
-        }
 
         var additionalSpends = AdditionalSpends.ToLookup(x => new {x.CreatedDate.Year, x.CreatedDate.Month});
         
@@ -73,34 +45,14 @@ public class TaxProfile : BaseEntity
         {
             var incomePerMonth = incomeGroup.Sum(x => x.Value);
 
-            var currentAdditionalSpends = additionalSpends[incomeGroup.Key];
+            var currentAdditionalSpends = additionalSpends[incomeGroup.Key].ToArray();
             var beforeTaxAdditionalSpends = currentAdditionalSpends.Where(x => x.AppliedBeforeTax).Sum(x => x.Amount);
 
             incomePerMonth -= beforeTaxAdditionalSpends;
 
-            var incomeTaxPerMonth = 0m;
-            if (incomeTax.IsPercentage)
-            {
-                incomeTaxPerMonth = (incomePerMonth * (decimal) incomeTax.Amount) / 100;
-            }
-            else
-            {
-                incomeTaxPerMonth = (decimal)incomeTax.Amount;
-            }
-
-            var socialTaxPerMonth = 0m;
-            if (socialTax != null)
-            {
-                if (socialTax.IsPercentage)
-                {
-                    socialTaxPerMonth = (incomePerMonth * (decimal) socialTax.Amount) / 100;
-                }
-                else
-                {
-                    socialTaxPerMonth = (decimal) socialTax.Amount;
-                }
-            }
-
+            var incomeTaxPerMonth = CalculateIncomeTax(incomeTax, incomePerMonth);
+            var socialTaxPerMonth = CalculateSocialTax(socialTax, incomePerMonth);
+            
             var incomePerMonthNet = incomePerMonth - (incomeTaxPerMonth + socialTaxPerMonth);
             
             var afterTaxAdditionalSpends = currentAdditionalSpends.Where(x => !x.AppliedBeforeTax).Sum(x => x.Amount);
@@ -117,5 +69,74 @@ public class TaxProfile : BaseEntity
         }
 
         return result;
+    }
+
+    private Tax GetIncomeTax(decimal totalIncome)
+    {
+        var incomeTaxes = Taxes.Where(x => x.TaxType == TaxType.IncomeTax).ToArray();
+        if (!incomeTaxes.Any())
+        {
+            throw new Exception("No income taxes were configured");
+        }
+        
+        var incomeTax = incomeTaxes.Where(x => x.AppliesBefore != null && x.AppliesBefore < totalIncome)
+            .OrderByDescending(x => x.AppliesBefore)
+            .FirstOrDefault();
+        
+        if (incomeTax == null)
+        {
+            incomeTax = incomeTaxes.FirstOrDefault();
+        }
+
+        return incomeTax;
+    }
+
+    private decimal CalculateIncomeTax(Tax incomeTax, decimal incomePerMonth)
+    {
+        var incomeTaxPerMonth = 0m;
+        if (incomeTax.IsPercentage)
+        {
+            incomeTaxPerMonth = (incomePerMonth * (decimal) incomeTax.Amount) / 100;
+        }
+        else
+        {
+            incomeTaxPerMonth = (decimal)incomeTax.Amount;
+        }
+
+        return incomeTaxPerMonth;
+    }
+
+    private decimal CalculateSocialTax(Tax? socialTax, decimal incomePerMonth)
+    {
+        var socialTaxPerMonth = 0m;
+
+        if (socialTax != null)
+        {
+            if (socialTax.IsPercentage)
+            {
+                socialTaxPerMonth = (incomePerMonth * (decimal) socialTax.Amount) / 100;
+            }
+            else
+            {
+                socialTaxPerMonth = (decimal) socialTax.Amount;
+            }
+        }
+        
+        return socialTaxPerMonth;
+    }
+
+    private void ApplyDateFilter(ref Income[] incomes, DateTime? from = null, DateTime? to = null)
+    {
+        if (from != null)
+        {
+            incomes = incomes.Where(x => x.CreatedDate.Month >= from.Value.Month 
+                                         && x.CreatedDate.Year == from.Value.Year).ToArray();
+        }
+
+        if (to != null)
+        {
+            incomes = incomes.Where(x => x.CreatedDate.Month <= to.Value.Month 
+                                         && x.CreatedDate.Year == to.Value.Year).ToArray();
+        }
     }
 }
