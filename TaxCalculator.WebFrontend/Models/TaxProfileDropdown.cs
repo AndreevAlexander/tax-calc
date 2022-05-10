a1068766333247b797ae25117dc59cc63220c1a4
Oleksandr Andreiev <Oleksandr.Andreiev@halliburton.com>
2022-05-10 19:34:58 +0200

    begin dashboard

---
+++ TaxCalculator.WebFrontend/Models/TaxProfileDropdown.cs
@@ -0,0 +1,8 @@
+﻿namespace TaxCalculator.WebFrontend.Models;
+
+public class TaxProfileDropdown
+{
+    public string Id { get; set; }
+
+    public string Name { get; set; }
+}
---
+++ TaxCalculator.WebFrontend/Validation/SelectedTaxValidationRule.cs
@@ -0,0 +1,27 @@
+﻿using TaxCalculator.Validation.Contracts;
+using TaxCalculator.Validation.Result;
+
+namespace TaxCalculator.WebFrontend.Validation;
+
+public class SelectedTaxValidationRule : IValidationRule
+{
+    public IEnumerable<ValidationResult> Validate(object? data, string propertyName, object? context = null)
+    {
+        var results = new List<ValidationResult>();
+        
+        if (data is string s)
+        {
+            var result = Guid.TryParse(s, out Guid guid);
+            if (!result)
+            {
+                results.Add(ValidationResult.Invalid("Tax profile should be selected"));
+            }
+        }
+        else if (data == null)
+        {
+            results.Add(ValidationResult.Invalid("Tax profile should be selected"));
+        }
+
+        return results;
+    }
+}
