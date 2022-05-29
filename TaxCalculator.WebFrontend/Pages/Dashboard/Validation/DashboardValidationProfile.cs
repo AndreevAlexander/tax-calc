c98ee28d06a6bb02a9026551587bf2dbbf1ff469
Oleksandr Andreiev <Oleksandr.Andreiev@halliburton.com>
2022-05-29 18:42:07 +0200

    validation for date picker

---
+++ TaxCalculator.WebFrontend/Pages/Dashboard/Validation/DashboardValidationProfile.cs
@@ -0,0 +1,22 @@
+﻿using TaxCalculator.Validation;
+using TaxCalculator.WebFrontend.Models;
+
+namespace TaxCalculator.WebFrontend.Pages.Dashboard.Validation;
+
+public class DashboardValidationProfile : ValidationProfile
+{
+    public DashboardValidationProfile()
+    {
+        ForModel<TaxProfileDropdownModel>(b =>
+        {
+            b.Property(p => p.TaxProfileId)
+                .WithCustomRule<TaxProfileDropdownValidationRule>();
+
+            b.Property(p => p.From)
+                .WithCustomRule<DateFromValidationRule>();
+
+            b.Property(p => p.To)
+                .WithCustomRule<DateToValidationRule>();
+        });
+    }
+}
---
+++ TaxCalculator.WebFrontend/Pages/Dashboard/Validation/DateToValidationRule.cs
@@ -0,0 +1,23 @@
+﻿using TaxCalculator.Validation.Contracts;
+using TaxCalculator.Validation.Result;
+using TaxCalculator.WebFrontend.Models;
+
+namespace TaxCalculator.WebFrontend.Pages.Dashboard.Validation;
+
+public class DateToValidationRule : IValidationRule
+{
+    public async Task<IEnumerable<ValidationResult>> ValidateAsync(object? data, object model, object? context = null)
+    {
+        var results = new List<ValidationResult>();
+        
+        if (model is TaxProfileDropdownModel dropdownModel && data is DateTime toDate)
+        {
+            if (toDate < dropdownModel.From)
+            {
+                results.Add(ValidationResult.Invalid("Date 'To' can not be less than 'From'"));        
+            }
+        }
+
+        return results;
+    }
+}
