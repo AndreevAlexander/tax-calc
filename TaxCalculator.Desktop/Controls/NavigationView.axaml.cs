d7f90f8042641b8cd32a13ce6d463e34d918b73e
Oleksandr Andreiev <oleksandr.andreiev@honeycombsoft.com>
2023-04-30 00:41:11 +0200

    begin work on the layout control

---
+++ TaxCalculator.Desktop/Controls/NavigationView.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace TaxCalculator.Desktop.Controls;
+
+public class NavigationView : ItemsControl
+{
+    public object Content
+    {
+        get => GetValue(ContentProperty);
+        set => SetValue(ContentProperty, value);
+    }
+
+    public static AvaloniaProperty<object> ContentProperty =
+        AvaloniaProperty.Register<NavigationView, object>(nameof(Content));
+}
