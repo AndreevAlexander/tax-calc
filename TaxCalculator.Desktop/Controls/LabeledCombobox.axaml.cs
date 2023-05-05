0ddfabb8a0226f6e5ab187b60b51642b3da6b71d
Oleksandr Andreiev <oleksandr.andreiev@honeycombsoft.com>
2023-05-05 15:33:59 +0200

    added custom labeled controls

---
+++ TaxCalculator.Desktop/Controls/LabeledCombobox.axaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace TaxCalculator.Desktop.Controls;
+
+public class LabeledCombobox : TemplatedControl
+{
+    private string _label;
+
+    private IEnumerable _items;
+
+    private object _selectedItem;
+    
+    public string Label
+    {
+        get => _label;
+        set => SetAndRaise(LabelProperty, ref _label, value);
+    }
+
+    public IEnumerable Items
+    {
+        get => _items;
+        set => SetAndRaise(ItemsProperty, ref _items, value);
+    }
+
+    public object SelectedItem
+    {
+        get => _selectedItem;
+        set => SetAndRaise(SelectedItemProperty, ref _selectedItem, value);
+    }
+    
+    public static readonly DirectProperty<LabeledCombobox, string> LabelProperty =
+        AvaloniaProperty.RegisterDirect<LabeledCombobox, string>(nameof(Label), comboBox => comboBox.Label,
+            (comboBox, value) => comboBox.Label = value);
+
+    public static readonly DirectProperty<LabeledCombobox, IEnumerable> ItemsProperty =
+        AvaloniaProperty.RegisterDirect<LabeledCombobox, IEnumerable>(nameof(Items), c => c.Items,
+            (c, v) => c.Items = v);
+
+    public static readonly DirectProperty<LabeledCombobox, object> SelectedItemProperty =
+        AvaloniaProperty.RegisterDirect<LabeledCombobox, object>(nameof(SelectedItem), c => c.SelectedItem,
+            (c, v) => c.SelectedItem = v);
+}
---
+++ TaxCalculator.Desktop/Controls/LabeledTextBox.axaml.cs
@@ -0,0 +1,57 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+
+namespace TaxCalculator.Desktop.Controls;
+
+public class LabeledTextBox : TemplatedControl
+{
+    private string _text;
+
+    private string _label;
+
+    private TextWrapping _textWrapping;
+
+    private bool _acceptReturns;
+    
+    public string Text
+    {
+        get => _text;
+        set => SetAndRaise(TextProperty, ref _text, value);
+    }
+
+    public string Label
+    {
+        get => _label;
+        set => SetAndRaise(LabelProperty, ref _label, value);
+    }
+
+    public TextWrapping TextWrapping
+    {
+        get => _textWrapping;
+        set => SetAndRaise(TextWrappingProperty, ref _textWrapping, value);
+    }
+
+    public bool AcceptReturns
+    {
+        get => _acceptReturns;
+        set => SetAndRaise(AcceptReturnsProperty, ref _acceptReturns, value);
+    }
+
+    public static readonly DirectProperty<LabeledTextBox, string> TextProperty =
+        AvaloniaProperty.RegisterDirect<LabeledTextBox, string>(nameof(Text), textBox => textBox.Text,
+            (textBox, value) => textBox.Text = value);
+
+    public static readonly DirectProperty<LabeledTextBox, string> LabelProperty =
+        AvaloniaProperty.RegisterDirect<LabeledTextBox, string>(nameof(Label), textBox => textBox.Label,
+            (textBox, value) => textBox.Label = value);
+
+    public static readonly DirectProperty<LabeledTextBox, TextWrapping> TextWrappingProperty =
+        AvaloniaProperty.RegisterDirect<LabeledTextBox, TextWrapping>(nameof(TextWrapping),
+            textBox => textBox.TextWrapping,
+            (textBox, value) => textBox.TextWrapping = value);
+
+    public static readonly DirectProperty<LabeledTextBox, bool> AcceptReturnsProperty =
+        AvaloniaProperty.RegisterDirect<LabeledTextBox, bool>(nameof(AcceptReturns), textBox => textBox.AcceptReturns,
+            (textBox, value) => textBox.AcceptReturns = value);
+}
